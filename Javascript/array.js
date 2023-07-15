53ed7d99dbbb8a7b482c043d3e995a96d4d18809
khoa-1606 <huynhkhoavo1606@gmail.com>
2023-07-15 17:32:43 +0700

    JS.2

---
+++ Javascript/array.js
@@ -0,0 +1,56 @@
+/*
+1. Tạo mảng
+    - Cách tạo
+    //c1:
+    var languages = [
+        'Javascript',
+        'PHP',
+        'C++',
+        'Java',
+    ];
+    console.log(languages);
+    // chỉ mục đc đánh tự động, bắt đầu từ số 0
+
+    //c2:
+    var languages2 = new Array(
+        'Javascript',
+        'PHP',
+        'C++',
+        'Java',
+    );
+    console.log(languages2);
+
+E.x: -----------------------------------------
+Tạo biến numbers và gán cho nó 1 array,
+array này có ít nhất 3 phần tử là các giá trị số.
+    var numbers = [1, 2, 3];
+----------------------------------------------
+
+    - Sử dụng các nào -> sd c1
+
+    - Kiểm tra data type
+    console.log(typeof languages);
+    
+    Kiểm tra cho cụ thể array
+    console.log(Array.isArray{});
+
+2. Truy xuất mảng
+    - Độ dài mảng
+    var languages = [
+        'Javascript',
+        'PHP',
+        'C++',
+        'Java',
+    ];
+    console.log(languages.length);
+
+    - Lấy phần tử theo index
+    var languages = [
+        'Javascript',
+        'PHP',
+        'C++',
+        'Java',
+    ];
+    console.log(languages[0]);
+    //output: Javascript
+*/
