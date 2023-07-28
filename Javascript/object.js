bf1a65a00c644da3dc8053569a2d2d39769690d8
khoa-1606 <huynhkhoavo1606@gmail.com>
2023-07-28 19:18:16 +0700

    JS.2.3

--- Javascript/object.js
+++ Javascript/object.js
@@ -25,7 +25,6 @@ console.log(myInfo.getAge()); //output: 26
 
 //myInfo.email = 'huynhkhoavo1606@gmail.com';
 
-console.log(myInfo);
 
 //Lấy giá trị - 2 cách
 console.log(myInfo.name); //cách 1
@@ -38,3 +37,17 @@ console.log(myInfo[myKey]);
 // Xóa
 delete myInfo.age;
 console.log(myInfo);
+
+/*
+Tạo biến student và gán cho nó 1 object, 
+object này có 3 keys để thể hiện tên, 
+tuổi và địa chỉ của sinh viên. 
+Các keys có tên là name, age và address.
+
+var student = {
+    name: "Khoa", // tên của sinh viên
+    age: 26, // tuổi của sinh viên
+    address: "123 ABC Street" // địa chỉ của sinh viên
+  };
+*/
+  
